9e07ffe4d8fce57a5690d84672d731bb5a6ff3d5
Vitaly Posadnev <elemabor@gmail.com>
2021-10-10 18:40:52 +0300

    feat(components): add CalculateComponent.vue with basic functionally

---
+++ src/components/CalculateComponent.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="mt-5">
+  <h3 class="text-center">{{name}}</h3>
+
+  <div class="input-group mb-3 text-center">
+    <input type="text" class="form-control" :placeholder="inputPlaceholder" v-model="sourceValue">
+    <span class="input-group-text">⇋</span>
+    <input type="text" class="form-control" :placeholder="outputPlaceholder" :value="convert">
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "CalculateComponent",
+  data: () => {
+    return {
+      sourceValue: 0.0,
+      convertedValue: 0.0,
+    }
+  },
+  props: {
+    name: String,
+    inputPlaceholder: String,
+    outputPlaceholder: String,
+    convertFunction: Function,
+  },
+  computed: {
+    convert () {
+      return this.convertFunction(this.sourceValue);
+    }
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  font-weight: bold;
+}
+</style>
